0e6d98b78105d5106832dfa8ebaf05f61cd2dc34
alpha-martinez <martinezalphaa@gmail.com>
2020-08-30 16:40:17 -0400

    [Starpage] got my star page basics up

---
+++ react-star-wars/src/StarPage.js
@@ -0,0 +1,17 @@
+import React from react;
+import React, {Component} from react;
+
+const StarPage = (props) => {
+    return (
+        <div> 
+            <h1>Details</h1>
+            <ul>
+                <li>
+                    name: 
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default StarPage;
