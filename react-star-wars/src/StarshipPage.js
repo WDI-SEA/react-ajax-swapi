15c439f61cbae29c891e02c5cc048928cc9f7853
alpha-martinez <martinezalphaa@gmail.com>
2020-08-30 19:58:53 -0400

    [style and details] added more details and style to the page

--- react-star-wars/src/StarshipPage.js
+++ react-star-wars/src/StarshipPage.js
@@ -7,6 +7,8 @@ const StarshipPage = (props) => {
         <div div="shipDetails" id="shipDetails"> 
             <h3>NAME: {props.location.state.name}</h3>
             <h3>MODEL: {props.location.state.model}</h3>
+            <h3>LENGTH: {props.location.state.length} meters</h3>
+            <h3>CARGO CAPACITY: {props.location.state.cargo_capacity}kg</h3>
             <button onClick={() => history.goBack()}>Return</button>
         </div>
     )
