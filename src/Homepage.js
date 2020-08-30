0c9c24e9a2381822c6023e21d680b228e0767213
blangwell <b.langwell@outlook.com>
2020-08-30 15:42:04 -0700

    [style/loading] Add styles and loading conditional logic in Homepage Component

--- src/Homepage.js
+++ src/Homepage.js
@@ -6,47 +6,47 @@ import {Link} from 'react-router-dom';
 
 const Homepage = (props) => {
   let [ships, setShips] = useState([])
-  // const location = useLocation();
+  let [loading, setLoading] = useState(true)
+
 
   useEffect(() => {
     let url = `https://swapi.dev/api/starships/`
     axios.get(url)
     .then(res => {
       console.log(res.data.results)
+      setLoading(false)
       setShips(res.data.results)
     })
     .catch (err => console.log(err))
   }, [])
 
-  // const allShips = props.ships.map((s, i) => {
-  //   console.log(s.name)
-  //   return <li key={i}>{s.name}</li>
-  // })
-  // if (!ships) {
-  //   return <h1>No dice</h1>
-  // }
-
-  return(
-    <div className='ship-container'>
-    {ships = ships.map((s, i) => {
-      return (
-        <div key={i} className='ship'>
-          <Link to={{
-            pathname: '/starship',
-            state: s
-          }}
-          key={i}
-          className='link-text'
-          >
-          {s.name} 
-          </Link>
+  if (!loading) {
+    return(
+      <div className='ship-container'>
+        {ships = ships.map((s, i) => {
+          return (
+            <div key={i} className='ship'>
+              <button className='butt'>
+              <Link to={{
+                pathname: '/starship',
+                state: s
+              }}
+              key={i} 
+              className='link-text'>
+              {s.name} 
+              </Link>
+              </button>
+            </div>
+          )
+        })}
       </div>
-      
     )
-    })}
 
-    </div>
-  )
+  } else {
+    return(
+      <h1 className='loading'>Loadin'</h1>
+    )
+  }
   
 }
 
