41cd639bd55159bc2604f8b5f79e443aa9b88b7f
alpha-martinez <martinezalphaa@gmail.com>
2020-08-30 20:49:44 -0400

    [loading] added part 3

--- react-star-wars/src/Homepage.js
+++ react-star-wars/src/Homepage.js
@@ -4,39 +4,45 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 const Homepage = () => {
-  const [ships, setShips] = useState([])
+  const [ships, setShips] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   
   useEffect(() => {
       let url = `https://swapi.dev/api/starships`
       axios.get(url)
       .then(res => {
-          console.log(res.data.results)
-          setShips(res.data.results)
+          console.log(res.data.results);
+          setLoading(false);
+          setShips(res.data.results);
       }).catch (err => (console.log(err, 'error')))
   }, [])
-
-  return (
-    <div className="s-container">
-         
-      {ships.map((s,i) => {
-        return (
-        <div className="shipList">  
-            <Link 
-                to={{
-                    pathname: 'starship',
-                    state: s
-                }}
-                    key={i}
-                >
-                <button className="name">{s.name}</button>
-            </Link>
-        </div>   
-              )
-          })}
-      </div>
-
-  )
-  
+  if(!loading) {
+    return (
+        <div className="s-container">
+             
+          {ships.map((s,i) => {
+            return (
+            <div className="shipList">  
+                <Link 
+                    to={{
+                        pathname: 'starship',
+                        state: s
+                    }}
+                        key={i}
+                    >
+                    <button className="name">{s.name}</button>
+                </Link>
+            </div>   
+                  )
+              })}
+          </div>
+      )
+  } else {
+      return (
+          <h1>Loading...</h1>
+      )
+  }
 }
 
 export default Homepage;
