e007119cc366e506aebad99f8c023ad062a19e80
kinawy <sameh.yasser.kinawy@gmail.com>
2020-08-31 01:11:55 -0400

    will fix in the morning

--- react-star-wars/src/Components/PilotList.js
+++ react-star-wars/src/Components/PilotList.js
@@ -2,18 +2,26 @@ import React, { useEffect } from "react";
 import axios from "axios";
 
 const PilotList = (props) => {
-  let pilotUrls = props.pilot;
-  console.log(props.pilots);
-  const getPilots = async (urls) => {
-    const getUrls = urls.map(async (url) => {
-      const response = await axios.get(pilotUrls).then((res) => {
-        return <p>hi</p>;
-      });
-      const pilots = await Promise.all(getUrls);
-      console.log("hey");
-    });
+  let pilotUrls = props.pilots;
+  const funcAsync = async () => {
+    const asyncUrls = await Promise.all(
+      pilotUrls.map(async (pilot, i) => {
+        try {
+          let results = await axios
+            .get(pilot)
+            .then((res) => {
+              return res;
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        } catch (error) {
+          console.log(error);
+        }
+      })
+    );
   };
-
+  funcAsync();
   return <div>{PilotList}</div>;
 };
 
